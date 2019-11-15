1c50b86a554156a14fef0682c71f3a8d7714632f
Blaineworld <40525428+Blaineworld@users.noreply.github.com>
2019-11-15 14:02:18 -0600

    Add files via upload
    I actually DIDN'T forget this project!

--- environment/compile.js
+++ environment/compile.js
@@ -1,3 +1,27 @@
 'use strict';
 
-console.log("This will compile your Engle files into Engle Declarative Binaries.");
+(async () => {
+	const fs = require("fs");
+
+	if (process.argv.length < 3) {
+		console.error("err: Please specify a source file.\nSyntax: (...) <source> [<destination>]\n");
+		process.exit();
+	}
+
+	const sourcePath = fs.existsSync(process.argv[2]) ? process.argv[2] : process.argv[2] + ".engle";
+	const destinationPath = process.argv.length > 3 ? process.argv[3] : sourcePath + ".bin";
+
+	try {
+		var source = await fs.promises.readFile(sourcePath, "utf8");
+	} catch(error) {
+		console.log(`err: The source file could not be opened! (${error.code})`);
+		process.exit();
+	}
+
+	try {
+		var D = await fs.promises.open(destinationPath, "a");
+	} catch(error) {
+		console.log(`err: The destination file could not be opened! (${error.code})`);
+		process.exit();
+	}
+})();
